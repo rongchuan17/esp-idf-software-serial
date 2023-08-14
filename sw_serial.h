dc85c7dc63c90288e6636049d239e15e0bb21874
容川 <89556779+rongchuan17@users.noreply.github.com>
2023-08-14 20:30:54 +0800

    Update sw_serial.h
    esp_clk.h has been replaced by esp32/clk.h, please include esp32/clk.h instead

--- sw_serial.h
+++ sw_serial.h
@@ -11,7 +11,7 @@ http://www.opensource.org/licenses/mit-license.php
 #include <freertos/task.h>
 #include <freertos/portmacro.h>
 
-#include <esp_clk.h>
+#include <esp32/clk.h>
 #include <driver/gpio.h>
 #include <soc/cpu.h>
 
