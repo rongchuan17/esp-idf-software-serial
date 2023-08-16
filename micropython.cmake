d05462801e1c2e9a6df5bce9af685df76e0d4f26
容川 <89556779+rongchuan17@users.noreply.github.com>
2023-08-16 15:22:20 +0800

    Update micropython.cmake

--- micropython.cmake
+++ micropython.cmake
@@ -3,7 +3,7 @@ add_library(usermod_cexample INTERFACE)
 
 # Add our source files to the lib
 target_sources(usermod_cexample INTERFACE
-    ${CMAKE_CURRENT_LIST_DIR}/examplemodule.c
+    ${CMAKE_CURRENT_LIST_DIR}/modserial.c
 )
 
 # Add the current directory as an include directory.
