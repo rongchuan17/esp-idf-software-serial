d3599f30fceeff3e78124a1254ad9669033dc140
rongchuan17 <89556779+rongchuan17@users.noreply.github.com>
2023-08-16 11:32:30 +0800

    Update micropython.cmake

--- micropython.cmake
+++ micropython.cmake
@@ -1,15 +1,15 @@
 # Create an INTERFACE library for our CPP module.
-add_library(serial_module INTERFACE)
+add_library(serial INTERFACE)
 
 # Add our source files to the library.
-target_sources(serial_module INTERFACE
+target_sources(serial INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/modserial.c
 )
 
 # Add the current directory as an include directory.
-target_include_directories(serial_module INTERFACE
+target_include_directories(serial INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}
 )
 
 # Link our INTERFACE library to the usermod target.
-target_link_libraries(usermod INTERFACE serial_module)
+target_link_libraries(usermod INTERFACE serial)
