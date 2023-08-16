e699883981db5eda03ca17dbea135a07c39d151c
rongchuan17 <89556779+rongchuan17@users.noreply.github.com>
2023-08-16 11:19:20 +0800

    test

--- micropython.cmake
+++ micropython.cmake
@@ -1,15 +1,15 @@
-# Create an INTERFACE library for our C module.
-add_library(usermod_cexample INTERFACE)
+# Create an INTERFACE library for our CPP module.
+add_library(serial_module INTERFACE)
 
-# Add our source files to the lib
-target_sources(usermod_cexample INTERFACE
-    ${CMAKE_CURRENT_LIST_DIR}/examplemodule.c
+# Add our source files to the library.
+target_sources(serial_module INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}/modserial.c
 )
 
 # Add the current directory as an include directory.
-target_include_directories(usermod_cexample INTERFACE
+target_include_directories(serial_module INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}
 )
 
 # Link our INTERFACE library to the usermod target.
-target_link_libraries(usermod INTERFACE usermod_cexample)
+target_link_libraries(usermod INTERFACE serial_module)
--- micropython.mk
+++ micropython.mk
@@ -1,9 +1,3 @@
 EXAMPLE_MOD_DIR := $(USERMOD_DIR)
-
-# Add all C files to SRC_USERMOD.
-SRC_USERMOD += $(EXAMPLE_MOD_DIR)/examplemodule.c
-
-# We can add our module folder to include paths if needed
-# This is not actually needed in this example.
-CFLAGS_USERMOD += -I$(EXAMPLE_MOD_DIR)
-CEXAMPLE_MOD_DIR := $(USERMOD_DIR)
+# Add our source files to the respective variables.
+SRC_USERMOD += $(EXAMPLE_MOD_DIR)/modeserial.c
