d0aa248d3b962e65cce53c0c871baf7d1cc23fc4
rongchuan17 <89556779+rongchuan17@users.noreply.github.com>
2023-08-16 11:10:12 +0800

    test02

--- micropython.mk
+++ micropython.mk
@@ -1,12 +1,9 @@
-CPPEXAMPLE_MOD_DIR := $(USERMOD_DIR)
+EXAMPLE_MOD_DIR := $(USERMOD_DIR)
 
-# Add our source files to the respective variables.
-SRC_USERMOD += $(CPPEXAMPLE_MOD_DIR)/examplemodule.c
-SRC_USERMOD_CXX += $(CPPEXAMPLE_MOD_DIR)/example.cpp
+# Add all C files to SRC_USERMOD.
+SRC_USERMOD += $(EXAMPLE_MOD_DIR)/examplemodule.c
 
-# Add our module directory to the include path.
-CFLAGS_USERMOD += -I$(CPPEXAMPLE_MOD_DIR)
-CXXFLAGS_USERMOD += -I$(CPPEXAMPLE_MOD_DIR) -std=c++11
-
-# We use C++ features so have to link against the standard library.
-LDFLAGS_USERMOD += -lstdc++
+# We can add our module folder to include paths if needed
+# This is not actually needed in this example.
+CFLAGS_USERMOD += -I$(EXAMPLE_MOD_DIR)
+CEXAMPLE_MOD_DIR := $(USERMOD_DIR)
