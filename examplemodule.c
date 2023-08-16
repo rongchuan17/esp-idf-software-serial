5da1fea25ce0a7994e2d80a9850f2f349be57736
容川 <89556779+rongchuan17@users.noreply.github.com>
2023-08-16 14:06:00 +0800

    Update examplemodule.c

--- examplemodule.c
+++ examplemodule.c
@@ -1,90 +1,119 @@
-// Include MicroPython API.
+/*
+Github: junhuanchen
+Copyright (c) 2018 Juwan
+Licensed under the MIT license:
+http://www.opensource.org/licenses/mit-license.php
+*/
+
+#include "sw_serial.h"
+
 #include "py/runtime.h"
-
-// Used to get the time in the Timer class example.
+#include "py/mperrno.h"
 #include "py/mphal.h"
+#include "py/nlr.h"	
 
-// This is the function which will be called from Python as cexample.add_ints(a, b).
-STATIC mp_obj_t example_add_ints(mp_obj_t a_obj, mp_obj_t b_obj) {
-    // Extract the ints from the micropython input objects.
-    int a = mp_obj_get_int(a_obj);
-    int b = mp_obj_get_int(b_obj);
-
-    // Calculate the addition and convert to MicroPython object.
-    return mp_obj_new_int(a + b);
+STATIC mp_obj_t serial_initialize() {
+    return mp_const_none;
 }
-// Define a Python reference to the function above.
-STATIC MP_DEFINE_CONST_FUN_OBJ_2(example_add_ints_obj, example_add_ints);
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(serial_initialize_obj, serial_initialize);
 
-// This structure represents Timer instance objects.
-typedef struct _example_Timer_obj_t {
-    // All objects start with the base.
-    mp_obj_base_t base;
-    // Everything below can be thought of as instance attributes, but they
-    // cannot be accessed by MicroPython code directly. In this example we
-    // store the time at which the object was created.
-    mp_uint_t start_time;
-} example_Timer_obj_t;
-
-// This is the Timer.time() method. After creating a Timer object, this
-// can be called to get the time elapsed since creating the Timer.
-STATIC mp_obj_t example_Timer_time(mp_obj_t self_in) {
-    // The first argument is self. It is cast to the *example_Timer_obj_t
-    // type so we can read its attributes.
-    example_Timer_obj_t *self = MP_OBJ_TO_PTR(self_in);
-
-    // Get the elapsed time and return it as a MicroPython integer.
-    mp_uint_t elapsed = mp_hal_ticks_ms() - self->start_time;
-    return mp_obj_new_int_from_uint(elapsed);
+STATIC mp_obj_t serial_new(size_t n_args, const mp_obj_t *args) {
+    if (n_args == 4) {
+        mp_obj_t TxPin = args[0], RxPin = args[1], Inverse = args[2], buffSize = args[3];
+        
+        /*
+        if (RxPin != XX) {
+            nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_Exception, 
+                "RxPin(arg 0) %d ", mp_obj_get_int(RxPin)));
+        }
+        */
+        
+        SwSerial *self = sw_new(mp_obj_get_int(TxPin), mp_obj_get_int(RxPin), mp_obj_get_int(Inverse), mp_obj_get_int(buffSize));
+        // printf("%d %d %d %d\n", mp_obj_get_int(TxPin), mp_obj_get_int(RxPin), mp_obj_get_int(Inverse), mp_obj_get_int(buffSize));
+        if (self != NULL) {
+            // sw_open(self, 115200);
+            // sw_stop(self);
+            return MP_OBJ_FROM_PTR(self);
+        }
+    }
+    return mp_const_none;
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_1(example_Timer_time_obj, example_Timer_time);
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(serial_new_obj, 0, 4, serial_new);
 
-// This represents Timer.__new__ and Timer.__init__, which is called when
-// the user instantiates a Timer object.
-STATIC mp_obj_t example_Timer_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args) {
-    // Allocates the new object and sets the type.
-    example_Timer_obj_t *self = mp_obj_malloc(example_Timer_obj_t, type);
-
-    // Initializes the time for this Timer instance.
-    self->start_time = mp_hal_ticks_ms();
-
-    // The make_new function always returns self.
-    return MP_OBJ_FROM_PTR(self);
+STATIC mp_obj_t serial_del(mp_obj_t self) {
+    SwSerial *tmp = (SwSerial *)self;
+    sw_del(tmp);
+    return mp_const_none;
 }
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(serial_del_obj, serial_del);
 
-// This collects all methods and other static class attributes of the Timer.
-// The table structure is similar to the module table, as detailed below.
-STATIC const mp_rom_map_elem_t example_Timer_locals_dict_table[] = {
-    { MP_ROM_QSTR(MP_QSTR_time), MP_ROM_PTR(&example_Timer_time_obj) },
+STATIC mp_obj_t serial_write(mp_obj_t self, mp_obj_t data) {
+    SwSerial *tmp = (SwSerial *)self;
+    
+    // printf("write %d \n", (uint8_t)mp_obj_get_int(data));
+    
+    sw_write(tmp, (uint8_t)mp_obj_get_int(data));
+    
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_2(serial_write_obj, serial_write);
+
+STATIC mp_obj_t serial_read(mp_obj_t self) {
+    SwSerial *tmp = (SwSerial *)self;
+    
+    int res = sw_read(tmp);
+    
+    // printf("read %d \n", (uint8_t)res);
+    
+    return mp_obj_new_int(res);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(serial_read_obj, serial_read);
+
+STATIC mp_obj_t serial_any(mp_obj_t self) {
+    SwSerial *tmp = (SwSerial *)self;
+    
+    int res = sw_any(tmp);
+    
+    if (res > 0)
+    {
+        return mp_obj_new_int(res);
+    }
+    
+    return mp_obj_new_int(0);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(serial_any_obj, serial_any);
+
+STATIC mp_obj_t serial_stop(mp_obj_t self) {
+    SwSerial *tmp = (SwSerial *)self;
+    return mp_obj_new_int(sw_stop(tmp));
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(serial_stop_obj, serial_stop);
+
+STATIC mp_obj_t serial_open(mp_obj_t self, mp_obj_t baudRate) {
+    SwSerial *tmp = (SwSerial *)self;
+    esp_err_t res = sw_open(tmp, mp_obj_get_int(baudRate));
+    return mp_obj_new_int(res);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_2(serial_open_obj, serial_open);
+
+STATIC const mp_rom_map_elem_t serial_module_globals_table[] = {
+    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_serial) },
+    { MP_ROM_QSTR(MP_QSTR___init__), MP_ROM_PTR(&serial_initialize_obj) },
+
+    { MP_ROM_QSTR(MP_QSTR_new), MP_ROM_PTR(&serial_new_obj) },
+    { MP_ROM_QSTR(MP_QSTR_del), MP_ROM_PTR(&serial_del_obj) },
+    { MP_ROM_QSTR(MP_QSTR_open), MP_ROM_PTR(&serial_open_obj) },
+    { MP_ROM_QSTR(MP_QSTR_stop), MP_ROM_PTR(&serial_stop_obj) },
+    { MP_ROM_QSTR(MP_QSTR_write), MP_ROM_PTR(&serial_write_obj) },
+    { MP_ROM_QSTR(MP_QSTR_read), MP_ROM_PTR(&serial_read_obj) },
+    { MP_ROM_QSTR(MP_QSTR_any), MP_ROM_PTR(&serial_any_obj) },
+    
+    { MP_ROM_QSTR(MP_QSTR_EOF), MP_ROM_INT((mp_int_t)SW_EOF)},
 };
-STATIC MP_DEFINE_CONST_DICT(example_Timer_locals_dict, example_Timer_locals_dict_table);
 
-// This defines the type(Timer) object.
-MP_DEFINE_CONST_OBJ_TYPE(
-    example_type_Timer,
-    MP_QSTR_Timer,
-    MP_TYPE_FLAG_NONE,
-    make_new, example_Timer_make_new,
-    locals_dict, &example_Timer_locals_dict
-    );
+STATIC MP_DEFINE_CONST_DICT(serial_module_globals, serial_module_globals_table);
 
-// Define all attributes of the module.
-// Table entries are key/value pairs of the attribute name (a string)
-// and the MicroPython object reference.
-// All identifiers and strings are written as MP_QSTR_xxx and will be
-// optimized to word-sized integers by the build system (interned strings).
-STATIC const mp_rom_map_elem_t example_module_globals_table[] = {
-    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_ctestexample) },
-    { MP_ROM_QSTR(MP_QSTR_add_ints), MP_ROM_PTR(&example_add_ints_obj) },
-    { MP_ROM_QSTR(MP_QSTR_Timer),    MP_ROM_PTR(&example_type_Timer) },
-};
-STATIC MP_DEFINE_CONST_DICT(example_module_globals, example_module_globals_table);
-
-// Define module object.
-const mp_obj_module_t example_user_cmodule = {
+const mp_obj_module_t serial_module = {
     .base = { &mp_type_module },
-    .globals = (mp_obj_dict_t *)&example_module_globals,
+    .globals = (mp_obj_dict_t*)&serial_module_globals,
 };
-
-// Register the module to make it available in Python.
-MP_REGISTER_MODULE(MP_QSTR_ctestexample, example_user_cmodule);
